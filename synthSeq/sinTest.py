f0df5c896d7670748f23a1932049a8064498c9d5
Nerve Corrosion <jackontheedge@gmail.com>
2023-03-23 20:10:45 +0000

    Notation and timing development

--- synthSeq/sinTest.py
+++ synthSeq/sinTest.py
@@ -9,13 +9,23 @@ phase_increment = 2 * math.pi * frequency / sample_rate  # Phase increment for e
 phase = 0  # Starting phase
 midline = 2.5  # Midline of the sine wave
 amplitude = 2.5  # Amplitude of the sine wave
+timeoffSet=0.0001
 
+values = []
+# time the function
+start = time.time()
 for i in range(samples):
     value = midline + amplitude * math.sin(phase)  # Calculate the value of the sine wave at the current phase with adjusted midline and amplitude
-    print(value)  # Output the value (you can replace this with your own processing)
+    # print(value)  # Output the value (you can replace this with your own processing)
+    # values.append(value)
     phase += phase_increment  # Increment the phase
-    time.sleep(1 / sample_rate)  # Sleep to ensure that the total duration is one second
+    time.sleep((1 / sample_rate)-timeoffSet)  # Sleep to ensure that the total duration is one second
 
+end = time.time()
 
-print(min(samples))
-print(max(samples))
+# print(min(values))
+# print(max(values))
+
+print("Total time:", end - start)
+print("Time selected:", duration)
+print("Time difference:", end - start - duration)
